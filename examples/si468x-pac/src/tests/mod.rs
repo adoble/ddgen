574203916ea2151962b39a399c77066d1af2c75c
Andrew Doble <andrew.l.doble@gmail.com>
2024-03-08 09:49:57 +0100

    Ignore the SPIDevice test

--- examples/si468x-pac/src/tests/mod.rs
+++ examples/si468x-pac/src/tests/mod.rs
@@ -6,6 +6,7 @@ use crate::get_sys_state::*;
 use crate::types::*;
 
 use embedded_hal::spi::SpiBus;
+use embedded_hal::spi::SpiDevice;
 use embedded_hal_mock::eh1::pin::{
     Mock as PinMock, State as PinState, Transaction as PinTransaction,
 };
@@ -13,7 +14,7 @@ use embedded_hal_mock::eh1::spi::{Mock as SpiMock, Transaction as SpiTransaction
 use embedded_hal_nb::spi::FullDuplex;
 
 #[test]
-fn an_spi_test() {
+fn spi_bus_test() {
     let expectations = [
         SpiTransaction::write(0x09),
         SpiTransaction::read(0x0A),
@@ -35,12 +36,48 @@ fn an_spi_test() {
 
     // Transferring
     let mut buf = vec![3, 4];
-    spi.transfer_in_place(&mut buf).unwrap();
+    //spi.transfer_in_place(&mut buf).unwrap();
+    SpiBus::transfer_in_place(&mut spi, &mut buf).unwrap();
     assert_eq!(buf, vec![5, 6]);
 
     // Finalise expectations
     spi.done();
 }
+
+// Ignoring this test as the code for embassy-rp  seems to  only supports SpiBus for embedded-hal v1.0.0
+// and not SpiDevice.
+#[test]
+#[ignore]
+fn spi_device_test() {
+    let expectations = [
+        SpiTransaction::write(0x09),
+        SpiTransaction::read(0x0A),
+        SpiTransaction::write(0xFE),
+        SpiTransaction::read(0xFF),
+        SpiTransaction::write_vec(vec![1, 2]),
+        SpiTransaction::transfer_in_place(vec![3, 4], vec![5, 6]),
+    ];
+
+    let mut spi = SpiMock::new(&expectations);
+    // FullDuplex transfers
+    FullDuplex::write(&mut spi, 0x09).unwrap();
+    assert_eq!(FullDuplex::read(&mut spi).unwrap(), 0x0A);
+    FullDuplex::write(&mut spi, 0xFE).unwrap();
+    assert_eq!(FullDuplex::read(&mut spi).unwrap(), 0xFF);
+
+    // Writing
+    SpiDevice::write(&mut spi, &vec![1, 2]).unwrap();
+
+    // Transferring
+    let mut buf = vec![3, 4];
+    //spi.transfer_in_place(&mut buf).unwrap();
+    SpiDevice::transfer_in_place(&mut spi, &mut buf).unwrap();
+    assert_eq!(buf, vec![5, 6]);
+
+    // Finalise expectations
+    spi.done();
+}
+
 #[test]
 fn integration_test_spi() {
     let spi_expectations = [
