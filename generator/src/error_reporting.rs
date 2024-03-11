02b03a087cdeaa274e287745cfffe417114c3486
Andrew Doble <andrew.l.doble@gmail.com>
2024-03-11 17:11:20 +0100

    Location added, use Span

--- generator/src/error_reporting.rs
+++ generator/src/error_reporting.rs
@@ -2,51 +2,87 @@ use std::ops::Range;
 
 use crossterm::style::Stylize;
 
-pub struct ContentRanges(Vec<Range<usize>>);
-impl ContentRanges {
-    fn from(contents: &str) -> ContentRanges {
-        let mut ranges: Vec<Range<usize>> = Vec::new();
+#[derive(Copy, PartialEq, Debug, Clone)]
+pub struct Span {
+    start: usize,
+    end: usize,
+}
+
+impl Span {
+    pub fn envelopes(&self, sub_span: Span) -> bool {
+        sub_span.start >= self.start && sub_span.end <= self.end
+    }
+}
+
+pub struct ContentSpans(Vec<Span>);
+impl ContentSpans {
+    fn from(contents: &str) -> ContentSpans {
+        let mut spans: Vec<Span> = Vec::new();
         let mut count = 0;
         let mut start: usize = 1;
         let mut end: usize = 0;
 
         for line in contents.lines() {
             end = end + line.len() + 1; // Includes the newline
-            let range = Range { start, end };
-            ranges.push(range);
+            let span = Span { start, end };
+            spans.push(span);
             start = end + 1;
         }
 
-        ContentRanges(ranges)
+        ContentSpans(spans)
+    }
+
+    pub fn location(&self, span: Span) -> Option<ErrorLocation> {
+        for (i, r) in self.0.iter().enumerate() {
+            if r.envelopes(span) {
+                let location = ErrorLocation {
+                    line: i + 1, // 0 based elements to 1 based lines
+                    start_column: span.start - r.start + 1,
+                    end_column: span.end - r.start + 1,
+                };
+                return Some(location);
+            }
+        }
+
+        None
     }
 }
+
+// // Is the sub-range really completely contained in the range.
+// fn range_is_subset(range: Range<usize>, sub_range: Range<usize>) -> bool {
+//     sub_range.start >= range.start && sub_range.end <= range.start
+// }
+
+#[derive(PartialEq, Eq, Clone, Copy, Debug)]
 pub struct ErrorLocation {
     line: usize,
     start_column: usize,
     end_column: usize,
 }
 
-pub fn error_report(contents: &str, message: &str, span: Option<Range<usize>>) {
-    let mut content_ranges = ContentRanges::from(contents);
-
-    match span {
-        Some(span) => {
-            let location = find_location(span);
-            println!(
-                "{} at line: {}, columns: {}..{}",
-                message, location.line, location.start_column, location.end_column
-            );
+pub fn error_report(contents: &str, message: &str, span_range: Option<Range<usize>>) {
+    let span = match span_range {
+        Some(range) => Span {
+            start: range.start,
+            end: range.end,
+        },
+        None => {
+            println!("{}", message);
+            return;
         }
+    };
+
+    let mut content_ranges = ContentSpans::from(contents);
+    let location = content_ranges.location(span);
+    match location {
+        Some(loc) => println!(
+            "{} at line: {}, columns: {}..{}",
+            message, loc.line, loc.start_column, loc.end_column
+        ),
         None => println!("{}", message),
     }
 
-    // let location = find_location(span);
-
     //println!("{} {:?}", message.red(), span);
-}
-
-fn find_location(span: Range<usize>) -> ErrorLocation {
-    todo!()
 }
 
 #[cfg(test)]
@@ -58,12 +94,41 @@ mod tests {
         //                   12345678 9012345678901 2345678901234567
         //                             1         2          3
 
-        let ranges = ContentRanges::from(content);
+        let spans = ContentSpans::from(content);
 
-        assert_eq!(ranges.0.len(), 3);
+        assert_eq!(spans.0.len(), 3);
 
-        assert_eq!(ranges.0[0], Range { start: 1, end: 8 });
-        assert_eq!(ranges.0[1], Range { start: 9, end: 21 });
-        assert_eq!(ranges.0[2], Range { start: 22, end: 38 });
+        assert_eq!(spans.0[0], Span { start: 1, end: 8 });
+        assert_eq!(spans.0[1], Span { start: 9, end: 21 });
+        assert_eq!(spans.0[2], Span { start: 22, end: 38 });
+    }
+
+    #[test]
+    fn test_location() {
+        let content = "This is\nsome content\nwith many lines.";
+        //                   12345678 9012345678901 2345678901234567
+        //                             1         2          3
+
+        let ranges = ContentSpans::from(content);
+
+        let search_range = Span { start: 12, end: 20 };
+        let location = ranges.location(search_range).unwrap();
+
+        assert_eq!(
+            location,
+            ErrorLocation {
+                line: 2,
+                start_column: 4,
+                end_column: 12
+            }
+        );
+    }
+
+    #[test]
+    fn test_sub_range() {
+        let span = Span { start: 4, end: 9 };
+        let sub_span = Span { start: 5, end: 7 };
+
+        assert!(span.envelopes(sub_span));
     }
 }
