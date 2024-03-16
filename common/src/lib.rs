2b2b1820e8deb2c3628b50cec00059e6e62c4a7d
Andrew Doble <andrew.l.doble@gmail.com>
2024-03-16 18:50:34 +0100

    Refactor

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,7 +1,43 @@
 mod response;
 
+// TODO extend with u16, u32 etc.
+pub fn repeating_words_u8<const LEN: usize>(
+    source: &[u8],
+    start: usize,
+    repeats: usize,
+) -> [u8; LEN] {
+    let mut buf: [u8; LEN] = [0; LEN];
+    let mut i = 0;
+
+    for b in &source[start..(start + repeats)] {
+        buf[i] = *b;
+        i += 1;
+    }
+
+    buf
+}
+
+// TODO extend with u16, u32 etc.
+pub fn repeating_words_u16<const LEN: usize>(
+    source: &[u8],
+    start: usize,
+    repeats: usize,
+) -> [u16; LEN] {
+    let mut buf: [u16; LEN] = [0; LEN];
+    let mut i = 0;
+
+    for b in source[start..].chunks(2).take(repeats) {
+        buf[i] = u16::from_le_bytes([b[0], b[1]]);
+        i += 1;
+    }
+
+    buf
+}
+
 #[cfg(test)]
 mod tests {
+
+    use super::*;
 
     use crate::response::ResponseWord;
     use bit_lang::{BitRange, BitSpec, Condition, Repeat as WordRepeat, Word};
@@ -429,39 +465,5 @@ mod tests {
         let d: [u16; 5] = repeating_words_u16(&data, w, data[counter_word].into());
 
         assert_eq!(d, expected);
-    }
-
-    // TODO extend with u16, u32 etc.
-    fn repeating_words_u8<const LEN: usize>(
-        source: &[u8],
-        start: usize,
-        repeats: usize,
-    ) -> [u8; LEN] {
-        let mut buf: [u8; LEN] = [0; LEN];
-        let mut i = 0;
-
-        for b in &source[start..(start + repeats)] {
-            buf[i] = *b;
-            i += 1;
-        }
-
-        buf
-    }
-
-    // TODO extend with u16, u32 etc.
-    fn repeating_words_u16<const LEN: usize>(
-        source: &[u8],
-        start: usize,
-        repeats: usize,
-    ) -> [u16; LEN] {
-        let mut buf: [u16; LEN] = [0; LEN];
-        let mut i = 0;
-
-        for b in source[start..].chunks(2).take(repeats) {
-            buf[i] = u16::from_le_bytes([b[0], b[1]]);
-            i += 1;
-        }
-
-        buf
     }
 }
