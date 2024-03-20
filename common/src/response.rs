de756a83e60bb19431b92c5838e2720d8aef4964
Andrew Doble <andrew.l.doble@gmail.com>
2024-03-20 18:53:17 +0100

    deserialize u8 and u32

--- common/src/response.rs
+++ common/src/response.rs
@@ -31,12 +31,27 @@ pub trait ResponseArray {
     fn deserialize_repeating_words(&mut self, source: &[u8]);
 }
 
+impl<const TARGET_LEN: usize> ResponseArray for [u8; TARGET_LEN] {
+    fn deserialize_repeating_words(&mut self, source: &[u8]) {
+        source.iter().enumerate().for_each(|(i, b)| self[i] = *b);
+    }
+}
+
 impl<const TARGET_LEN: usize> ResponseArray for [u16; TARGET_LEN] {
     fn deserialize_repeating_words(&mut self, source: &[u8]) {
         source
             .chunks(2)
             .enumerate()
             .for_each(|(i, b)| self[i] = u16::from_le_bytes([b[0], b[1]]));
+    }
+}
+
+impl<const TARGET_LEN: usize> ResponseArray for [u32; TARGET_LEN] {
+    fn deserialize_repeating_words(&mut self, source: &[u8]) {
+        source
+            .chunks(4)
+            .enumerate()
+            .for_each(|(i, b)| self[i] = u32::from_le_bytes([b[0], b[1], b[2], b[3]]));
     }
 }
 
@@ -62,18 +77,32 @@ mod tests {
 
     #[test]
     fn test_u16_array() {
-        let source: [u8; 10] = [0, 0, 0xCE, 0x56, 0x35, 0x82, 0, 0, 0, 0]; // Contains other data
+        let expected_data_u8: [u8; 2] = [12, 13];
+        let expected_data_u16: [u16; 7] = [22222, 33333, 0, 0, 0, 0, 0];
+        let expected_data_u32: [u32; 2] = [4_200_000_000, 3_333_333_333];
 
-        let expected_data: [u16; 7] = [22222, 33333, 0, 0, 0, 0, 0];
+        let source: [u8; 14] = [
+            12, 13, 0xCE, 0x56, 0x35, 0x82, 0x00, 0xEA, 0x56, 0xFA, 0x55, 0xA1, 0xAE, 0xC6,
+        ];
 
         struct A {
-            data: [u16; 7], // 7 is the max number of repeats
+            data_u8: [u8; 2],
+            data_u16: [u16; 7], // 7 is the max number of repeats
+            data_u32: [u32; 2],
         }
         let mut a = A {
-            data: [0; 7], // 7 is the max number of repeats
+            data_u8: [0; 2],
+            data_u16: [0; 7], // 7 is the max number of repeats
+            data_u32: [0; 2],
         };
+
+        a.data_u8.deserialize_repeating_words(&source[0..2]);
+        assert_eq!(a.data_u8, expected_data_u8);
         // Location is slice range and number of repeats is derived from start_word_index + (bit_slice->repeats) / (word range)
-        a.data.deserialize_repeating_words(&source[2..6]);
-        assert_eq!(a.data, expected_data);
+        a.data_u16.deserialize_repeating_words(&source[2..6]);
+        assert_eq!(a.data_u16, expected_data_u16);
+
+        a.data_u32.deserialize_repeating_words(&source[6..]);
+        assert_eq!(a.data_u32, expected_data_u32);
     }
 }
