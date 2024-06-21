a1b672a7aa989f95329b0ffc825f73a9721fd7de
Andrew Doble <andrew.l.doble@gmail.com>
2024-06-21 18:06:21 +0200

    Generates a polled send

--- generator/src/command.rs
+++ generator/src/command.rs
@@ -134,8 +134,48 @@ impl Command {
         let cased_request_name = request_name.to_case(Case::UpperCamel);
         let cased_response_name = response_name.to_case(Case::UpperCamel);
 
+        match &self.flow_control {
+            FlowControl::Direct => self.generate_direct_send(
+                tokens,
+                &cased_request_name,
+                &cased_response_name,
+                common_structures,
+            ),
+            FlowControl::Polled { on, condition } => self.generate_polled_send(
+                tokens,
+                &cased_request_name,
+                &cased_response_name,
+                common_structures,
+                on,
+                condition,
+            ),
+        };
+
+        // quote_in!(*tokens =>
+        //     impl $(cased_request_name.clone()) {
+        //         // This needs to be generated as we need to corrected specifiy the sizes
+        //         // of the request and response.
+        //         pub fn send<SPI: SpiDevice>(&self, spi: &mut SPI) -> Result<$(cased_response_name), DeviceError> {
+        //             const REQUEST_BUF_LEN: usize = $(self.request.buffer_size(common_structures));
+        //             const RESPONSE_BUF_LEN: usize = $(self.response.buffer_size(common_structures));
+
+        //             let response = self.transmit::<REQUEST_BUF_LEN, RESPONSE_BUF_LEN>(spi)?;
+        //             Ok(response)
+        //         }
+        //     }
+        // );
+    }
+
+    // Generate the send funtion for a DIRECT flow control
+    fn generate_direct_send(
+        &self,
+        tokens: &mut Tokens<Rust>,
+        cased_request_name: &str,
+        cased_response_name: &str,
+        common_structures: &HashMap<String, CommonStructure>,
+    ) {
         quote_in!(*tokens =>
-            impl $(cased_request_name.clone()) {
+            impl $(cased_request_name) {
                 // This needs to be generated as we need to corrected specifiy the sizes
                 // of the request and response.
                 pub fn send<SPI: SpiDevice>(&self, spi: &mut SPI) -> Result<$(cased_response_name), DeviceError> {
@@ -146,8 +186,41 @@ impl Command {
                     Ok(response)
                 }
             }
-        );
+        )
     }
+
+    // Generate the send function for a POLLED flow control.
+    fn generate_polled_send(
+        &self,
+        tokens: &mut Tokens<Rust>,
+        cased_request_name: &str,
+        cased_response_name: &str,
+        common_structures: &HashMap<String, CommonStructure>,
+        on: &str,
+        condition: &str,
+    ) {
+        let header_structure = common_structures.get(on).unwrap(); //TODO Error handling
+        let header_structure_buf_size = header_structure.buffer_size();
+        let cased_header_structure_name = on.to_case(Case::UpperCamel);
+        let request_buf_size = self.request.buffer_size(common_structures);
+        let response_buf_size = self.response.buffer_size(common_structures);
+
+        quote_in!(*tokens =>
+            impl $cased_request_name {
+            pub fn send<SPI: SpiDevice>(&self, spi: &mut SPI) -> Result<$cased_response_name, DeviceError> {
+                let f = | h: $(cased_header_structure_name.clone())  | h.$condition;
+
+                const REQUEST_BUF_LEN: usize = $request_buf_size;
+                const RESPONSE_BUF_LEN: usize = $response_buf_size;
+                const STATUS_HEADER_LEN: usize = $header_structure_buf_size;
+
+                let response = self.polled_transmit::<REQUEST_BUF_LEN, RESPONSE_BUF_LEN, $cased_header_structure_name, STATUS_HEADER_LEN>(spi, f)?;
+
+                Ok(response)
+            }
+        })
+    }
+
     pub fn providers(&self) -> impl Iterator<Item = String> {
         let providers: Vec<String> = self
             .request
